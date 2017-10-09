4d1a8ae5376701e66bf0d2ee3db19959116f4723
Bobby Priambodo <bobby.priambodo@gmail.com>
2017-10-09 09:53:46 +0700

    Implement `one` and `all` query

--- lib/ezpostgresql.ml
+++ lib/ezpostgresql.ml
@@ -1,7 +1,31 @@
+type connection = Postgresql.connection
+
 let connect ~conninfo =
   Lwt_preemptive.detach (fun () ->
       new Postgresql.connection ~conninfo ()
     )
 
-let create_pool ~conninfo ~size () =
-  Lwt_pool.create size (connect ~conninfo)
+let one ~query ?(params=[||]) conn =
+  Lwt_preemptive.detach (fun (c : connection) ->
+      let result = c#exec ~expect:[Postgresql.Tuples_ok] ~params query in
+      result#get_tuple 0
+    ) conn
+
+let all ~query ?(params=[||]) conn =
+  Lwt_preemptive.detach (fun (c : connection) ->
+      let result = c#exec ~expect:[Postgresql.Tuples_ok] ~params query in
+      result#get_all
+    ) conn
+
+module Pool = struct
+
+  let create ~conninfo ~size () =
+    Lwt_pool.create size (connect ~conninfo)
+
+  let one ~query ?(params=[||]) pool =
+    Lwt_pool.use pool (one ~query ~params)
+
+  let all ~query ?(params=[||]) pool =
+    Lwt_pool.use pool (all ~query ~params)
+
+end
--- test/test.ml
+++ test/test.ml
@@ -1,22 +1,63 @@
-let conninfo = "host=localhost"
+let conninfo = "host=localhost dbname=mydb"
 
 let tests = [
 
   "connect", [
     Alcotest_lwt.test_case "could connect" `Quick (fun _ _ ->
-        let%lwt c = Ezpostgresql.connect ~conninfo () in
-        Lwt.return @@ Alcotest.(check (string)) "same string" "localhost" c#host
+        let%lwt conn = Ezpostgresql.connect ~conninfo () in
+        Lwt.return @@ Alcotest.(check (string)) "same string" "localhost" conn#host
       )
   ];
 
-  "create_pool", [
+  "one", [
+    Alcotest_lwt.test_case "could run `one` query" `Quick (fun _ _ ->
+        let%lwt conn = Ezpostgresql.connect ~conninfo () in
+        let%lwt res =
+          Ezpostgresql.one ~query:"
+            SELECT * FROM person WHERE name = $1
+          " ~params:[| "Bobby" |] conn in
+        Lwt.return @@ Alcotest.(check (string)) "same string" "Bobby" (res.(0))
+      )
+  ];
+
+  "all", [
+    Alcotest_lwt.test_case "could run `all` query" `Quick (fun _ _ ->
+        let%lwt conn = Ezpostgresql.connect ~conninfo () in
+        let%lwt res = Ezpostgresql.all ~query:"
+          SELECT * FROM person
+        " conn in
+        Lwt.return @@ Alcotest.(check (int)) "same string" 2 (Array.length res)
+      )
+  ];
+
+  "Pool.create", [
     Alcotest_lwt.test_case "could use connection from pool" `Quick (fun _ _ ->
-        let pool = Ezpostgresql.create_pool ~conninfo ~size:10 () in
+        let pool = Ezpostgresql.Pool.create ~conninfo ~size:10 () in
         Lwt_pool.use pool (fun c ->
             Lwt.return @@ Alcotest.(check (string)) "same string" "localhost" c#host
           )
       )
-  ]
+  ];
+
+  "Pool.one", [
+    Alcotest_lwt.test_case "could run `one` query using pool" `Quick (fun _ _ ->
+        let pool = Ezpostgresql.Pool.create ~conninfo ~size:10 () in
+        let%lwt res = Ezpostgresql.Pool.one ~query:"
+          SELECT * FROM person WHERE name = $1
+        " ~params:[| "Bobby" |] pool in
+        Lwt.return @@ Alcotest.(check (string)) "same string" "Bobby" (res.(0))
+      )
+  ];
+
+  "Pool.all", [
+    Alcotest_lwt.test_case "could run `all` query using pool" `Quick (fun _ _ ->
+        let pool = Ezpostgresql.Pool.create ~conninfo ~size:10 () in
+        let%lwt res = Ezpostgresql.Pool.all ~query:"
+          SELECT * FROM person
+        " pool in
+        Lwt.return @@ Alcotest.(check (int)) "same string" 2 (Array.length res)
+      )
+  ];
 
 ]
 
